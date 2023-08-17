e6537a153131ffcc8198b0587df3efb6797840c1
88anandlok <113242383+88anandlok@users.noreply.github.com>
2023-08-17 16:09:28 +0530

    Add files via upload
    Added java file

---
+++ src/com/demo/A.java
@@ -0,0 +1,22 @@
+Class A {
+        Public void M1()throws IOException{
+        Sop("Hi in A")}}
+
+        Class B {
+        Public void M1(){
+        Sop("Hi in B")}
+        Class c extends A,B{
+        Public void demo(){
+        A a=nw A():
+        a.M1();
+        B b=new B();
+        b.M1();
+
+        Sop("hi in c")}
+
+        Public static void main(args[])
+        {
+        C c=new C();
+        c.demo();
+        }
+        }
