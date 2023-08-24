0d5251f1714f1181cd9364b6aa28a69424c5e1c4
88anandlok <113242383+88anandlok@users.noreply.github.com>
2023-08-24 12:37:57 +0530

    Update Demo.java for testing of Demo2 branch

--- src/com/demo/Demo.java
+++ src/com/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Generic Demo");
 		System.out.println("Lead Stmt");
+		System.out.println("Demo2 branch testing");
 	}
 
 }
